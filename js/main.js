21acd59b44722ae0eddc3f91dd8e4b651b8a12aa
david cortesi <david.cortesi.097@gmail.com>
2020-10-21 17:35:51 +0200

    commit 3

--- js/main.js
+++ js/main.js
@@ -24,11 +24,22 @@ bottoneGenera.addEventListener( "click" , function(){
   }else if ( ( eta < 1) && ( eta > 130) ){
     alert("ATTENZIONE INSERIRE VALORI ETA' VALIDI");
   }
+  //Numeri treno e carrozza casuali
+  var cp = Math.floor(Math.random() * (10000 - 9000)) + 9000;
+  document.getElementById("cp").innerHTML = cp;
+
+  var carrozza = Math.floor( Math.random() *(10 - 1) ) + 1;
+  document.getElementById("carrozza").innerHTML = carrozza;
+  
   document.getElementById("prezzo").innerHTML = prezzo + " €";
 } )
-//Numeri treno e carrozza casuali
-var cp = Math.floor(Math.random() * (10000 - 9000)) + 9000;
-document.getElementById("cp").innerHTML = cp;
 
-var carrozza = Math.floor( Math.random() *(10 - 1) ) + 1;
-document.getElementById("carrozza").innerHTML = carrozza;
+bottoneAnnulla.addEventListener( "click", function(){
+  var nome = document.getElementById("nome").value = "";
+  var cognome = document.getElementById("cognome").value = "" ;
+  var km = document.getElementById("km").value = "";
+  var eta = document.getElementById("eta").value = "";
+  document.getElementById("cp").innerHTML = "";
+  document.getElementById("carrozza").innerHTML = "";
+  document.getElementById("prezzo").innerHTML = "";
+})
